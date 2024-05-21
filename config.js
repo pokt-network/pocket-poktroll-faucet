36374ee680e21bf9feded3a72fa3ea3397c83641
stakenodes <151650486+stakenodes@users.noreply.github.com>
2024-05-21 18:52:18 -0400

    modified to adjust blockchains property values
    - Set `tx.amount` to `20000`
    - Set `fee.amount` to a `null` array
    - Set `fee.gas` to 100000
    - Set `limit` to `2`

--- config.js
+++ config.js
@@ -45,116 +45,21 @@ export default {
                 amount: [
                     {
                         denom: "upokt",
-                        amount: "10000000000"
+                        amount: "20000"
                     },
-                    // {
-                    //     denom: "uatom",
-                    //     amount: "10000000000"
-                    // },
                 ],
                 fee: {
-                    amount: [
-                        {
-                            amount: "0",
-                            denom: "upokt"
-                        }
-                    ],
-                    gas: "0"
+                    amount: [],
+                    gas: "100000"
                 },
             },
             limit: {
                 // how many times each wallet address is allowed in a window(24h)
-                address: 1, 
+                address: 2,
                 // how many times each ip is allowed in a window(24h),
                 // if you use proxy, double check if the req.ip is return client's ip.
-                ip: 10 
+                ip: 10
             }
         },
-        // {
-        //     type: 'Ethermint',
-        //     ids: {
-        //         chainId: 1818,
-        //         cosmosChainId: 'sidechain_1818-1',
-        //     },
-        //     name: "Proxima",
-        //     endpoint: {
-        //         // make sure that CORS is enabled in rpc section in config.toml
-        //         // cors_allowed_origins = ["*"]
-        //         rpc_endpoint: "https://proxima-rpc.side.exchange",
-        //         evm_endpoint: "http://13.229.237.39:8545/",
-        //     },
-        //     sender: {
-        //         mnemonic,
-        //         option: {
-        //             hdPaths: [stringToPath("m/44'/60")],
-        //             prefix: "prox"
-        //         }
-        //     },
-        //     tx: {
-        //         amount: {
-        //             denom: "aprox",
-        //             amount: "5000000000000000000"
-        //         },
-        //         fee: {
-        //             amount: [
-        //                 {
-        //                     amount: "100000",
-        //                     denom: "aprox"
-        //                 }
-        //             ],
-        //             gas: "10000000000000"
-        //         },
-        //     },
-        //     limit: {
-        //         // how many times each wallet address is allowed in a window(24h)
-        //         address: 1, 
-        //         // how many times each ip is allowed in a window(24h),
-        //         // if you use proxy, double check if the req.ip is return client's ip.
-        //         ip: 10 
-        //     }
-        // },
-        // {
-        //     type: 'Ethermint',
-        //     ids: {
-        //         chainId: 1819,
-        //         cosmosChainId: 'sidechain_1819-1',
-        //     },
-        //     name: "Toliman",
-        //     endpoint: {
-        //         // make sure that CORS is enabled in rpc section in config.toml
-        //         // cors_allowed_origins = ["*"]
-        //         rpc_endpoint: "https://toliman-rpc.side.exchange",
-        //         evm_endpoint: "http://52.77.209.10:8545/",
-        //     },
-        //     sender: {
-        //         mnemonic,
-        //         option: {
-        //             hdPaths: [stringToPath("m/44'/60/0'/0/0")],
-        //             prefix: "toli"
-        //         }
-        //     },
-        //     tx: {
-        //         amount: {
-        //             denom: "atoli",
-        //             amount: "5000000000000000000"
-        //         },
-        //         fee: {
-        //             amount: [
-        //                 {
-        //                     amount: "100000",
-        //                     denom: "atoli"
-        //                 }
-        //             ],
-        //             gas: "10000000000000"
-        //         },
-        //     },
-        //     limit: {
-        //         // how many times each wallet address is allowed in a window(24h)
-        //         address: 1, 
-        //         // how many times each ip is allowed in a window(24h),
-        //         // if you use proxy, double check if the req.ip is return client's ip.
-        //         ip: 10 
-        //     }
-        // },
     ]    
 }
