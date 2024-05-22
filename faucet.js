f7a09b67762ba0018b407c470753dadbe753ceba
Muhammad Ehsan <syedehsans1@gmail.com>
2024-05-22 20:33:23 +0500

    Updated index.ejs to use txAmount from config

--- faucet.js
+++ faucet.js
@@ -41,6 +41,7 @@ app.get('/config.json', async (req, res) => {
   const project = conf.project
   project.sample = sample
   project.blockchains = conf.blockchains.map(x => x.name)
+  project.txAmount = conf.blockchains[0].tx?.amount[0]?.amount
   res.send(project);
 })
 
