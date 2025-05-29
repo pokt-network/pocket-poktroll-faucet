401dc5feb53caaac962169cd635c28f4dac690b2
Muhammad Ehsan <syedehsans1@gmail.com>
2025-05-29 21:13:46 +0500

    updated error message on mact request

--- faucet.js
+++ faucet.js
@@ -126,7 +126,7 @@ app.get('/send/:chain/:address', async (req, res) => {
         res.send({ result: `err: ${err}` });
       }
     } else {
-      res.send({ result: "This account has already been initialized with 1 MACT. Additional requests are not allowed." });
+      res.send({ result: "You’ve already claimed 1 MACT." });
     }
   } catch (err) {
     console.error(err);
